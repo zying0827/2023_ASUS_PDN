ff4ade2dd5fb2b627abbe2bb62e7a5ad4d44a8ea
leotseng <leotseng@eda.ee.ntu.edu.tw>
2023-09-25 12:35:30 +0800

    first commit

---
+++ app/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Printer.h"
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+    Printer printer;
+    printer.print();
+}
---
+++ build/app/CMakeFiles/testgit.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/leotseng/test_github/app/main.cpp" "app/CMakeFiles/testgit.dir/main.cpp.o" "gcc" "app/CMakeFiles/testgit.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/leotseng/test_github/build/src/CMakeFiles/source.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
